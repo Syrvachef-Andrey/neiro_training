d9dba60a35dfb7e655bfd5d724913a7263d0aca5
andrey <an.sirvacheff@yandex.ru>
2024-12-02 21:11:57 +0500

    hotfix

--- main_code/find_object.py
+++ main_code/find_object.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("/runs/classify/train2/weights/best.pt")
+model = YOLO("best.pt")
+# тут если надо модель изменить
 
 results = model("/home/andrey/PycharmProjects/neiro_training/test_dir/test_img.png")
 # тут изменить путь до файла
