129513b9476a00d3268d87390f525095c2bf214a
andrey <an.sirvacheff@yandex.ru>
2024-12-02 21:04:03 +0500

    feat: add readme

--- main_code/find_object.py
+++ main_code/find_object.py
@@ -3,3 +3,4 @@ from ultralytics import YOLO
 model = YOLO("/runs/classify/train2/weights/best.pt")
 
 results = model("/home/andrey/PycharmProjects/neiro_training/test_dir/test_img.png")
+# тут изменить путь до файла
